84928595de24c8310c4d19d20b7f63d30fc1ca75
Itay Shkliar <itayshkliar@gmail.com>
2026-06-01 10:55:20 +0300

    implement client to cpp server and fix docker

---
+++ web/src/services/cppClient.js
@@ -0,0 +1,59 @@
+import net from 'net';
+
+const HOST = process.env.CPP_SERVICE_HOST || 'cpp-service';
+const PORT = process.env.CPP_SERVICE_PORT || 8080;
+
+export function send(command) {
+    return new Promise((resolve, reject) => {
+
+        const client = new net.Socket();
+        let response = '';
+
+        client.connect(PORT, HOST, () => {
+            client.write(command + '\n');
+        });
+
+        client.on('data', (data) => {
+            response += data.toString();
+        });
+
+        client.on('close', () => {
+            resolve(response.trim());
+        });
+
+        client.on('error', reject);
+    });
+}
+
+// exported API
+export const cppClient = {
+
+    // when creating user, no watched products yet
+    createUser(userId) {
+        return send('POST ${userId}');
+    },
+
+    addView(userId, productId) {
+        return send(`PATCH ${userId} ${productId}`);
+    },
+
+    // multiple products. usage exmpaple: await cppClient.addViews(5, [12, 13, 14, 20]);
+    addViews(userId, productIds) {
+        const list = productIds.join(' ');
+        return send(`PATCH ${userId} ${list}`);
+    },
+
+    removeView(userId, productId) {
+        return send(`DELETE ${userId} ${productId}`)
+    },
+
+    // multiple products. usage exmpaple: await cppClient.removeViews(5, [12, 13, 14, 20]);
+    removeViews(userId, productIds) {
+        const list = productIds.join(' ');
+        return send(`DELETE ${userId} ${list}`)
+    },
+
+    getRecommendations(userId, productId) {
+        return send(`RECOMMEND ${userId} ${productId}`);
+    }
+};
